d8d65d740a7d1e2380d12c0c2bf45b6cd089d432
jugahy <jugahy1205@gmail.com>
2022-06-08 22:04:35 +0900

    initial commit

---
+++ 이것이취업을위한코딩테스트다/Realization/시각.py
@@ -0,0 +1,14 @@
+# My Code
+def solution(N):
+    count = 0
+
+    for i in range(N+1):
+        for j in range(60):
+            for k in range(60):
+                if "3" in str(i) + str(j) + str(k):
+                    count += 1
+    return count
+
+
+
+
