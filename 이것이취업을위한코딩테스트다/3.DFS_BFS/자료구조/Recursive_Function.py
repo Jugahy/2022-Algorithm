a61ea61801ea2351a9778a198e36e572c7ceb12e
jugahy <jugahy1205@gmail.com>
2022-06-10 22:55:11 +0900

    initial commit

---
+++ 이것이취업을위한코딩테스트다/3.DFS_BFS/자료구조/Recursive_Function.py
@@ -0,0 +1,25 @@
+# 재귀 함수
+
+
+# basic of recursive function
+def recursive():
+    print("재귀 함수는 자기 자신을 불러옵...")
+    recursive()
+
+
+# My Code
+def factorial(n):
+    answer = 1
+
+    while n != 1:
+        answer *= n
+        n -= 1
+
+    return answer
+
+
+# Good Code (use recursive function)
+def factorial1(n):
+    if n == 1:
+        return 1
+    return n * factorial1(n - 1)
